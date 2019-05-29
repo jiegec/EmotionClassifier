42b9c956c171aa8550084cb0f229bdc314d149c4
Jiajie Chen <noc@jiegec.ac.cn>
2019-05-29 16:45:46 +0800

    A cnn that over fits

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ skip_words = set(["a", "href", "nbsp", "href", "http:", "news", "sina", "com", "
 vocab_set = set()
 
 if torch.cuda.is_available():
-    device = torch.device("cuda:0")
+    device = torch.device("cuda:%s" % sys.argv[3])
 else:
     device = torch.device("cpu")
 
@@ -74,7 +74,7 @@ for line in test_file:
             words_len += 1
     test_data.append((torch.tensor([words], dtype=torch.long).to(device), torch.tensor(max_vote, dtype=torch.long)))
 
-batch_size = 32
+batch_size = 256
 batch_input_data = []
 input_data = []
 for i in range(0, len(data), batch_size):
@@ -93,21 +93,23 @@ for i in range(0, len(data), batch_size):
 
 print('got', len(data), 'news and', len(vocab), 'words')
 
-embedding_size = 128
-
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_size).to(device)
-        self.conv1 = nn.Conv1d(128, 128, 5).to(device)
-        self.conv2 = nn.Conv1d(128, 128, 5).to(device)
-        self.max1 = nn.MaxPool1d(5).to(device)
-        self.max2 = nn.MaxPool1d(5).to(device)
-        self.linear = nn.Linear(128 * 162, num_emotions).to(device)
+        self.embedding = nn.Embedding(vocab_size, 256).to(device)
+        self.conv1 = nn.Conv1d(256, 64, 20).to(device)
+        self.conv2 = nn.Conv1d(256, 64, 10).to(device)
+        self.conv3 = nn.Conv1d(256, 64, 5).to(device)
+        self.max1 = nn.MaxPool1d(32).to(device)
+        self.max2 = nn.MaxPool1d(32).to(device)
+        self.max3 = nn.MaxPool1d(32).to(device)
+        self.dropout = nn.Dropout(0.2).to(device)
+        self.linear = nn.Linear(32 * 762, num_emotions).to(device)
 
         nn.init.xavier_uniform(self.embedding.weight)
         nn.init.xavier_uniform(self.conv1.weight)
         nn.init.xavier_uniform(self.conv2.weight)
+        nn.init.xavier_uniform(self.conv3.weight)
         nn.init.xavier_uniform(self.linear.weight)
 
     def forward(self, x):
@@ -115,29 +117,38 @@ class CNN(nn.Module):
         #print(x.size())
         x = x.permute(0, 2, 1)
         #print(x.size())
-        x = F.relu(self.conv1(x))
+        x1 = F.relu(self.conv1(x))
         #print(x.size())
-        x = F.relu(self.max1(x))
+        x1 = F.relu(self.max1(x1))
+        #print(x1.size())
+        x2 = F.relu(self.conv2(x))
         #print(x.size())
-        x = F.relu(self.conv2(x))
+        x2 = F.relu(self.max2(x2))
+        #print(x2.size())
+        x3 = F.relu(self.conv3(x))
         #print(x.size())
-        x = F.relu(self.max2(x))
+        x3 = F.relu(self.max3(x3))
+        #print(x3.size())
+        x = torch.cat([x1, x2, x3])
+        x = F.relu(self.dropout(x))
         #print(x.size())
         x = x.view(1, -1)
         #print(x.size())
         x = self.linear(x)[0]
-        x = F.softmax(x)
         return x
 
 model = CNN()
 criterion = nn.CrossEntropyLoss().to(device)
 #criterion = nn.CrossEntropyLoss(weight=torch.tensor(max_vote_count, dtype=torch.float)).to(device)
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+#optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
 def print_accuracy():
     # on training set
     correct = 0
     wrong = 0
+    input_dist = [0] * num_emotions
+    output_dist = [0] * num_emotions
     for data in input_data:
         inputs, labels = data
         outputs = model(inputs)
@@ -145,13 +156,16 @@ def print_accuracy():
         input_ans = labels
         output_ans = np.argmax(list(outputs))
 
+        input_dist[input_ans] += 1
+        output_dist[output_ans] += 1
+
         #print('expected %d, got %d %s' % (input_ans, output_ans, outputs.tolist()))
         if input_ans == output_ans:
             correct += 1
         else:
             wrong += 1
 
-    print('train correct/all: %d/%d' % (correct, correct + wrong))
+    print('train correct/all: %d/%d, dist: %s %s' % (correct, correct + wrong, input_dist, output_dist))
 
     # on test set
     correct = 0
@@ -172,7 +186,7 @@ def print_accuracy():
     print('test correct/all: %d/%d' % (correct, correct + wrong))
 
 
-
+print("before training")
 print_accuracy()
 for epoch in range(1000):
     total_loss = 0.0
